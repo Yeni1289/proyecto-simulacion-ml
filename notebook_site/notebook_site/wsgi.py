56d4844fb5cc99b59b9e26480192894b3994fd0f
Yeni1289 <154487288+Yeni1289@users.noreply.github.com>
2026-01-11 23:59:28 -0600

    Update wsgi.py

--- notebook_site/notebook_site/wsgi.py
+++ notebook_site/notebook_site/wsgi.py
@@ -1,5 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notebook_site.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE',
+    'notebook_site.settings'
+)
+
 application = get_wsgi_application()
